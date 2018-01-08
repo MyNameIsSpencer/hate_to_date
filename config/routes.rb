ea9d06445d81bd8abe3c1b159ac0f769048c4d70
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-01-08 13:57:08 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :users, except: [:index] do
+    resources :results, only: [:index, :show]
+  end
+  resources :surveys, only: [:index, :show]
+  resource :session, only: [:new, :create, :destroy]
+
+
+
 end
