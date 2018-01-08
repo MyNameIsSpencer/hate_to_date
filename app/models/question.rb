917ae3a158664103a3a24137adcd6e2945c8cf57
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-01-08 16:25:32 -0500

    added fake seed data and custom validation for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,11 @@
 class Question < ApplicationRecord
   validates :option_a, :option_b, presence: true
-  validates :option_a, uniqueness: { scope: :option_b }
-
+  validate  :a_not_b
+    def a_not_b
+      if option_a == option_b
+        errors.add(:options, 'Options must be different')
+      end
+    end
 
   belongs_to :survey
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 
 
 User.destroy_all
-Restaurant.destroy_all
-Reservation.destroy_all
+Survey.destroy_all
+Question.destroy_all
 
 
 
 fsa_list = ['M3A',	'M4A',	'M5A',	'M6A',	'M7A',	'M9A',
-'M1B',	'M3B',	'M4B',	'M5B',	'M6B'	'M9B'
-'M1C',	'M3C',	'M4C',	'M5C',	'M6C'	'M9C'
+'M1B',	'M3B',	'M4B',	'M5B',	'M6B',	'M9B',
+'M1C',	'M3C',	'M4C',	'M5C',	'M6C',	'M9C',
 'M1E',	'M4E',	'M5E',	'M6E',
 'M1G',	'M4G',	'M5G',	'M6G',
 'M1H',	'M2H',	'M3H',	'M4H',	'M5H',	'M6H',
@@ -33,18 +33,27 @@ fsa_list = ['M3A',	'M4A',	'M5A',	'M6A',	'M7A',	'M9A',
 'M4Y',	'M7Y',	'M8Y',
 'M8Z']
 
-walla = User.create(name: 'Walla', email: 'wallaha@w..co', password: '1234', gender: 'female', phone: '1234567890', fsa: fsa_list.sample)
-puter = User.create(name: 'Puter', email: 'pupu@ter.co', password: '1234', gender: 'male', phone: '7563625243', fsa: fsa_list.sample)
-miketa = User.create(name: 'Mike Ta', email: 'miket@tata.co', password: '1234', gender: 'male', phone: '0096847566', fsa: fsa_list.sample)
-rulshshsh = User.create(name: 'Rulshshsh', email: 'rul@shsh.co', password: '1234', gender: 'male', phone: '2253648881', fsa: fsa_list.sample)
-hammerhead = User.create(name: 'Hammer Head', email: 'hammer@time.co', password: '1234', gender: 'female', phone: '1110002228', fsa: fsa_list.sample)
-zedder = User.create(name: '@##!!-z', email: '...@zi...', password: '1234', gender: 'female', phone: '8489069473', fsa: fsa_list.sample)
-beatrices = User.create(name: 'BEATRICES!', email: 'be@trice.co', password: '1234', gender: 'female', phone: '1829384756', fsa: fsa_list.sample)
-awesomeduck = User.create(name: 'Awesome Duck26', email: 'awesome@duck.co', password: '1234', gender: 'male', phone: '8844775563', fsa: fsa_list.sample)
-winchester = User.create(name: 'WinchESTer', email: 'winch@ester.co', password: '1234', gender: 'male', phone: '0192837465', fsa: fsa_list.sample)
-classic = User.create(name: 'cLASSIC', email: 'clas@sic', password: '1234', gender: 'female', phone: '3339994852', fsa: fsa_list.sample)
-dasher = User.create(name: '--__--', email: '-!-__-!-', password: '1234', gender: '', phone: '8883746666', fsa: fsa_list.sample)
+20.times do
+  user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.free_email,
+    password: 'password',
+    password_confirmation: 'password',
+    phone: 1234567890
+  )
+end
 
+Survey.create!(name: 'Pets')
+Survey.create!(name: 'Toys')
+Survey.create!(name: 'Sports')
+Survey.create!(name: 'Movies')
+Survey.create!(name: 'People')
 
-
+30.times do
+  question = Question.create!(
+    option_a: Faker::Friends.character,
+    option_b: Faker::Friends.location,
+    survey_id: Survey.all.ids.sample
+  )
+end
 # bootom
