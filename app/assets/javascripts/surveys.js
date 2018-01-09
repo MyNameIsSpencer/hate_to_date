b4bd0f2ffee1a7955df75b58cb358eda89a9de6f
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-09 11:32:41 -0500

    updated survey to only show 1 image pait and next pair on subsequent clicks, also added class names to images

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -3,29 +3,38 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 document.addEventListener("DOMContentLoaded", function() {
   var button = document.querySelector('button')
+  var questionCounter = 0
+  var imageHolder = document.createElement('div')
   button.addEventListener('click', function(event){
     event.preventDefault()
+    imageHolder.innerHTML = ""
     $.ajax({
-      url: "https://api.giphy.com/v1/gifs/search?api_key=RS4WEditBF6Ig0jzs1zJWbqTha5EPUo0&q=" + key + "&rating=g" ,
+      url: "https://api.giphy.com/v1/gifs/search?api_key=RS4WEditBF6Ig0jzs1zJWbqTha5EPUo0&q=" + key[questionCounter] + "&rating=g" ,
       method: 'GET',
       dataType: 'json'
     }).done(function(data){
-      var imageHolder = document.createElement('div')
       var image1 = document.createElement('img')
+      image1.className = "left"
       image1.src = data["data"][Math.floor(Math.random()*data["data"].length)]["images"]["fixed_height_still"]["url"];
+      image1.addEventListener('click', choose)
       $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=RS4WEditBF6Ig0jzs1zJWbqTha5EPUo0&q=" + value + "&rating=g" ,
+        url: "https://api.giphy.com/v1/gifs/search?api_key=RS4WEditBF6Ig0jzs1zJWbqTha5EPUo0&q=" + value[questionCounter] + "&rating=g" ,
         method: 'GET',
         dataType: 'json'
       }).done(function(data2){
         var image2 = document.createElement('img')
+        image2.className = "right"
         image2.src = data2["data"][Math.floor(Math.random()*data["data"].length)]["images"]["fixed_height_still"]["url"];
+        image2.addEventListener('click', choose)
         imageHolder.append(image1)
         imageHolder.append(image2)
         document.body.append(imageHolder)
+        questionCounter ++
       })
     })
   })
-
+  var choose = function makeSelection(){
+    console.log(this.className);
+  }
 
 });
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,17 +2,9 @@ class SurveysController < ApplicationController
 
   def show
     @survey = Survey.find(params[:id])
-    @result = {}
-    @test_results = {}
+    @results = {}
     @survey.questions.each do |question|
-      res1 = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["PokeGif_Key"]}&q=#{question.option_a}&rating=g")
-      body1 = JSON.parse(res1.body)
-      res2 = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["PokeGif_Key"]}&q=#{question.option_b}&rating=g")
-      body2 = JSON.parse(res2.body)
-      gif_url1 = body1["data"][rand(0..body1["data"].length-1)]["images"]["fixed_height_still"]["url"]
-      gif_url2 = body2["data"][rand(0..body2["data"].length-1)]["images"]["fixed_height_still"]["url"]
-      @result[gif_url1] = gif_url2
-      @test_results[question.option_a] = question.option_b
+      @results[question.option_a] = question.option_b
     end
   end
 
