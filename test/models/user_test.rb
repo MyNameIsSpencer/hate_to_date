171512a3a11bfa1b771d837b46f11c74e8d87a0e
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-01-08 20:38:05 -0500

    CHANGED minitest version from 5.11.1 to 5.9.1  IN Gemfile.lock to RESOLVE STACK TRACE ISSUE ON FAILURE

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "email must be unique" do
+
+  test "email must be unique refute negative" do
     user = create(:user, email: "bettymaker@gmail.com")
     user2 = build(:user, email: "bettymaker@gmail.com")
+    user3 = build(:user, email: "heytherebuddy@gggmailer.co")
     refute user2.valid?
+    assert user3.valid?
   end
+
 
   test "user must include password on create" do
     user = build(:user, password_digest: nil)
