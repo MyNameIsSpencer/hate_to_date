c094b904c575cc04b01ce7c630fbfa8cb6427bf1
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-12 02:02:16 -0500

    completed methods for question option uniqueness

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -15,18 +15,37 @@ class QuestionTest < ActiveSupport::TestCase
     refute question2.valid?
     assert question3.valid?
   end
-  #
-  # test "option_b is present" do
-  #   question = build(:question, option_a: nil)
-  #   question2 = build(:question, option_a: '')
-  #   question3 = build(:question, option_a: 'hello', option_b: 'goodbye')
-  #   # refute question.valid?
-  #   # refute question2.valid?
-  #   assert question3.valid?
-  # e
-  # end
-  #
-  # test "option_a and option_b are different" do
+
+  test "option_b is present" do
+    question = build(:question, option_a: nil)
+    question2 = build(:question, option_a: '')
+    question3 = build(:question, option_a: 'hello', option_b: 'goodbye', survey_id: 1)
+    refute question.valid?
+    refute question2.valid?
+    assert question3.valid?
+
+  end
+
+  test "option_a and option_b are different" do
+    question = build(:question, option_a: "East", option_b: "West", survey_id: 1)
+    question2 = build(:question, option_a: "West", option_b: "West", survey_id: 1)
+
+    assert question.valid?
+    refute question2.valid?
+  end
+
+  test 'options unique through both collumns' do
+    question = build(:question, option_a: "Best", option_b: "Test", survey_id: 1)
+    question2 = build(:question, option_a: "Chest", option_b: "Vest", survey_id: 1)
+    question3 = build(:question, option_a: "Vest", option_b: "Crest", survey_id: 1)
+    question4 = build(:question, option_a: "West", option_b: "Mest", survey_id: 1)
+
+    refute question.valid?
+    refute question2.valid?
+    refute question3.valid?
+    refute question4.valid?
+
+  end
   #
   # end
 end
