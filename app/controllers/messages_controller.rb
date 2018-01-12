17b8fcf32bfa57a6ca13d4be62df177f6bf83472
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-11 19:19:38 -0500

    added city to ticketmaster api request

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   def find_event
     @topic = params[:topic]
     @city = "toronto"
-    @ticket_master_event = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?keyword=#{@topic}&city=&apikey=#{ENV["Discovery_Key"]}")
+    @ticket_master_event = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?keyword=#{@topic}&city=#{@city}&apikey=#{ENV["Discovery_Key"]}")
     respond_to do |format|
       format.html
       format.json { render :json => @ticket_master_event }
