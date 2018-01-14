c82dcb769b04112c75ab95a913efa5c20075f8c6
bluepeartree <sherrie.sebastian@gmail.com>
2018-01-13 23:20:15 -0500

    created two step login functionality

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,3 +4,31 @@
 function initMap() {
   console.log('init Map');
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+console.log("what");
+var register = document.querySelector('.register');
+var modal = document.querySelector('.modal');
+var modal2 = document.querySelector('.modal2');
+var next = document.querySelector('.next');
+// to display sign in block
+register.addEventListener('click', function()
+{
+  modal.style.display = 'block';
+  register.style.display = 'none'
+}
+)
+
+next.addEventListener('click', function(e)
+{
+  console.log("ok");
+  modal.style.display = 'none';
+  modal2.style.display = 'block';
+  e.preventDefault();
+}
+)
+
+
+
+
+})
--- app/models/fsa.rb
+++ app/models/fsa.rb
@@ -1,11 +1,11 @@
 class Fsa < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: {is: 3}
+  # validates :name, presence: true
+  # validates :name, length: {is: 3}
 
   has_many :users
 
-  geocoded_by :fsa_to_latitude_longitude
-  after_validation :geocode
+  # geocoded_by :fsa_to_latitude_longitude
+  # after_validation :geocode
 
   def fsa_to_latitude_longitude
     return self.name
