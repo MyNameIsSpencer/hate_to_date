d8afb4f33f848dbbfe0f0fd075e0f8b3553c0db1
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-15 16:09:27 -0500

    removed load_picture from survey controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -11,18 +11,4 @@ end
     @user= current_user
   end
 
-  def load_pictures
-    @survey = Survey.find_by(id: params[:survey_id])
-    @pictures = []
-    left_pic = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["Giphy_Key"]}&q=#{@survey.questions[params[:number].to_i].option_a}&rating=g")
-    right_pic = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["Giphy_Key"]}&q=#{@survey.questions[params[:number].to_i].option_b}&rating=g")
-    @pictures << left_pic
-    @pictures << right_pic
-    @pictures << @survey.questions.length
-    respond_to do |format|
-      format.html
-      format.json { render :json => @pictures }
-    end
-  end
-
 end
