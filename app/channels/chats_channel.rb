77ba297824492461549297500e70acba1cc3592f
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-18 18:23:48 -0500

    added output for chat

--- app/channels/chats_channel.rb
+++ app/channels/chats_channel.rb
@@ -9,6 +9,7 @@ class ChatsChannel < ApplicationCable::Channel
   def receive(data)
     sender = User.find(params[:room][0].to_i)
     receiver = User.find(params[:room][1].to_i)
+    puts data
     unless receiver.blocks.include?(sender.id)
       ActionCable.server.broadcast("chat_#{@room[0]}_#{@room[1]}", data)
     else
