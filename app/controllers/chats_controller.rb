fa9f47f6c2e75c1b0b612c753770fcd0deeda52c
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-18 10:55:13 -0500

    added redirect if user tries to chat with deleted profile

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,13 +2,18 @@ class ChatsController < ApplicationController
 
   def chat_room
     @current_user = current_user
-    @receiver = User.find_by(id: params[:receiver])
-    @messages = Message.where(["user_id = ? and receiver_id = ? OR user_id = ? and receiver_id = ?", current_user.id, params[:receiver].to_i, params[:receiver].to_i, current_user.id]).order(created_at: :asc).pluck(:body, :user_id, :created_at)
-    @messages.map {|m| m[1]=User.find(id=m[1]).name}
+    if User.find_by(id: params[:receiver])
+      @receiver = User.find_by(id: params[:receiver])
+      @messages = Message.where(["user_id = ? and receiver_id = ? OR user_id = ? and receiver_id = ?", current_user.id, params[:receiver].to_i, params[:receiver].to_i, current_user.id]).order(created_at: :asc).pluck(:body, :user_id, :created_at)
+      @messages.map {|m| m[1]=User.find(id=m[1]).name}
 
-    respond_to do |format|
-      format.html
-      format.json { render :json => {message: @messages, sender: @current_user} }
+      respond_to do |format|
+        format.html
+        format.json { render :json => {message: @messages, sender: @current_user} }
+      end
+    else
+      flash[:notice] = "Sorry, that user has deleted their Profile"
+      redirect_to user_matches_url(@current_user.id)
     end
   end
 end
