06bebd2a93c465d62a04a2cd4a1ae91653f7ea60
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-25 19:34:20 -0500

    cleaned up controller code

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,7 +6,6 @@ class ChatsController < ApplicationController
       @receiver = User.find_by(id: params[:receiver])
       @messages = Message.where(["user_id = ? and receiver_id = ? OR user_id = ? and receiver_id = ?", current_user.id, params[:receiver].to_i, params[:receiver].to_i, current_user.id]).order(created_at: :asc).pluck(:body, :user_id, :created_at, :id, :read)
       @messages.map {|m| m[1]=User.find(id=m[1]).name}
-
       respond_to do |format|
         format.html
         format.json { render :json => {message: @messages, sender: @current_user} }
